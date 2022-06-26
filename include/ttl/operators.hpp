fa145458c3972ccb73e78e0a3b6114dedfc1075b
Luke Dalessandro <ldalessa@uw.edu>
2022-06-26 07:49:19 -0700

    Implement tensor traits for integral and floating_point types.

--- include/ttl/operators.hpp
+++ include/ttl/operators.hpp
@@ -2,10 +2,35 @@
 
 #include "ttl/product.hpp"
 #include "ttl/sum.hpp"
+#include "ttl/tensor_traits.hpp"
 #include "ttl/utils.hpp"
+#include <concepts>
 
 namespace ttl
 {
+    template <class T>
+    requires std::integral<T> or std::floating_point<T>
+    struct tensor_traits<T>
+    {
+        using scalar_type = T;
+
+        static consteval auto get_order() -> int {
+            return 0;
+        }
+
+        static constexpr auto get_extents(T) -> array<int, 0> {
+            return {};
+        }
+
+        static constexpr auto evaluate(T const& t, scalar_index<0> const&) -> T {
+            return t;
+        }
+
+        static constexpr auto evaluate(T& t, scalar_index<0> const&) -> T& {
+            return t;
+        }
+    };
+
     inline namespace operators
     {
         template <is_expression A, is_expression B>
--- tests/test.cpp
+++ tests/test.cpp
@@ -4,12 +4,12 @@
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
-// static_assert(ttl::concepts::scalar<int>);
-// static_assert(ttl::concepts::tensor<int>);
-// static_assert(ttl::concepts::expression<int>);
-// static_assert(ttl::concepts::scalar<double>);
-// static_assert(ttl::concepts::tensor<double>);
-// static_assert(ttl::concepts::expression<double>);
+static_assert(ttl::is_scalar<int>);
+static_assert(ttl::is_tensor<int>);
+static_assert(ttl::is_expression<int>);
+static_assert(ttl::is_scalar<double>);
+static_assert(ttl::is_tensor<double>);
+static_assert(ttl::is_expression<double>);
 // static_assert(ttl::concepts::scalar<ttl::expressions::bind<int, ttl::tensor_index<0>{}>>);
 // static_assert(ttl::concepts::tensor<ttl::expressions::bind<int, ttl::tensor_index<0>{}>>);
 // static_assert(ttl::concepts::expression<ttl::expressions::bind<int, ttl::tensor_index<0>{}>>);
@@ -18,29 +18,18 @@
 
 using scalar = ttl::tests::static_tensor<double, std::array<int, 0>{}>;
 static_assert(ttl::is_tensor<scalar>);
-// static_assert(ttl::concepts::scalar<scalar>);
-// static_assert(ttl::concepts::tensor<scalar>);
+static_assert(ttl::is_scalar<scalar>);
 static_assert(ttl::is_expression<scalar>);
-// static_assert(ttl::concepts::has_static_extents<scalar>);
-// static_assert(ttl::concepts::has_static_outer<scalar>);
 
 using vector3d = ttl::tests::static_tensor<double, std::array<int, 1>{3}>;
 static_assert(ttl::is_tensor<vector3d>);
+static_assert(not ttl::is_scalar<vector3d>);
 static_assert(not ttl::is_expression<vector3d>);
-// static_assert(not ttl::concepts::scalar<vector3d>);
-// static_assert(ttl::concepts::tensor<vector3d>);
-// static_assert(not ttl::concepts::expression<vector3d>);
-// static_assert(ttl::concepts::has_static_extents<vector3d>);
-// static_assert(not ttl::concepts::has_static_outer<vector3d>);
 
 using matrixNd = ttl::tests::dynamic_tensor<double, 2>;
 static_assert(ttl::is_tensor<matrixNd>);
+static_assert(not ttl::is_scalar<matrixNd>);
 static_assert(not ttl::is_expression<matrixNd>);
-// static_assert(not ttl::concepts::scalar<matrixNd>);
-// static_assert(ttl::concepts::tensor<matrixNd>);
-// static_assert(not ttl::concepts::expression<matrixNd>);
-// static_assert(not ttl::concepts::has_static_extents<matrixNd>);
-// static_assert(not ttl::concepts::has_static_outer<matrixNd>);
 
 static_assert(ttl::is_int_array<std::array<int, 0>>);
 static_assert(ttl::is_int_array<std::array<int, 1>>);
