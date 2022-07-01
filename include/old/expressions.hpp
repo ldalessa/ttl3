82da5a975b16cb2453dcf639e2ecbbdf4f79b114
Luke D'Alessandro <ldalessa@uw.edu>
2022-07-01 16:04:08 -0700

    Remove the old headers.

--- include/old/expressions.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "ttl/expressions/Bind.hpp"
-#include "ttl/expressions/Product.hpp"
-#include "ttl/expressions/Sum.hpp"
--- include/old/utils/expect.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef expect
-#include "ttl/utils/error.hpp"
-#define expect(...) (__builtin_expect((__VA_ARGS__), 1) || ttl::utils::error(#__VA_ARGS__))
-#endif
